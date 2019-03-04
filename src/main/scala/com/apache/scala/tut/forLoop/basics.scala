38dfa69bbc875126e15935c5ff0285d6b1b86c79
himanshu.chandra <himanshu.chandra09@gmail.com>
2019-03-04 17:29:13 +0530

    added 2-D array in for loop

--- src/main/scala/com/apache/scala/tut/forLoop/basics.scala
+++ src/main/scala/com/apache/scala/tut/forLoop/basics.scala
@@ -31,10 +31,22 @@ object basics {
 
     println("for loop for assigning value to a variable--START");
     var data=List("1","2","3","himanshu");
-    val vr = for{ i <- data if (i=="himanshu" || i=="1" | i=="2")} yield i
+    val vr = for{ i <- data if (i=="himanshu" || i=="1" || i=="2")} yield i
     println(vr)
+    println("for loop for assigning value to a variable--END");
 
-    println("for loop for assigning value to a variable--END")
+    println("2-D array handling--START");
+    val twoDimensionalArray = Array.ofDim[String](2,2)
+    twoDimensionalArray(0)(0) = "flour"
+    twoDimensionalArray(0)(1) = "sugar"
+    twoDimensionalArray(1)(0) = "egg"
+    twoDimensionalArray(1)(1) = "syrup"
+
+    for { x <- 0 until 2
+          y <- 0 until 2
+        } println(s"Donut ingredient at index ${(x,y)} is: ${twoDimensionalArray(x)(y)}");
+    println("2-D array handling--END");
+
 
 
   }
